d47358de2f9bfccd70796e3698cb9167d41d057f
ZacHooper <zac.g.hooper@gmail.com>
2021-08-05 14:36:50 +1000

    Scraped data

---
+++ scraper/README.md
@@ -0,0 +1,16 @@
+# Vic Baby Names Scraper
+
+It is not possible to download the full dataset in one go so I will need to scrape all the data down from the API and store it in a datafile locally. The dataset is from 2008 to 2020 so there is 12 years of data. Names are either MALE or FEMALE so there should be 2600 (13 actual recorded years) rows of data. 
+
+To keep the scraper simple it will make a request to the API for the top 100 names for a single gender in a single year. Theoretically that should mean 26 requests to the API. The API is limited to 25 requests per minute so the whole scrape time should take just over a minute. 
+
+## Flow
+1. Start with year 2008
+2. Make a request to the API for the top 100 MALE names
+3. Store the returned results in a list (possibly Pandas DataFrame)
+4. Make a request to the API for the top 100 FEMALE names 
+5. Store the returned results in a list (possibly Pandas DataFrame)
+6. Repeat until all years have been retrieved
+
+## MISC Notes
+- In order to be a _good_ API user a rate limit has been used on the API function to ensure that we don't exceed the 25 requests a minute.
--- scraper/run.py
+++ scraper/run.py
@@ -1,1 +1,39 @@
+from requests.api import get
 import mysecrets
+import requests
+import pandas as pd
+import json
+from ratelimit import limits, sleep_and_retry
+
+
+URL = "https://wovg-community.gateway.prod.api.vic.gov.au/bdm/names/v1.0/popular-baby-names"
+
+@sleep_and_retry
+@limits(calls=25, period=60)
+def get_top_100_baby_names(gender, year):
+    """Get's the top 100 baby names for the given gender and year. Returns the names a list of dicts.
+    Each item will detail the _position_ the name was ranked, the _name_, the _count_ of how many children were given the name, the _sex_ of the child with the name and the _year_ the name was given.  
+
+    Args:
+        gender (str): Either MALE or FEMALE
+        year (int): an Int between 2008 & 2020
+    """
+    r = requests.get(URL, params={"sex":gender,"year":year}, headers={"apikey": mysecrets.API_KEY})
+    data = r.json()
+    return data['popular_baby_names']
+
+total_list = []
+for year in range(2008, 2021):
+    print(f"Getting names for {year}")
+    # Make API call for MALE
+    male_names = get_top_100_baby_names("MALE", year)
+
+    # Make API call for FEMALE
+    female_names = get_top_100_baby_names("FEMALE", year)
+
+    # Add data to total list
+    total_list += male_names
+    total_list += female_names
+
+# Write total list to a CSV file
+df = pd.DataFrame(total_list).to_csv('data.csv')
